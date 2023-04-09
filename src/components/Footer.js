852241f6036236b3a3784323023feabd28b77038
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-04-09 12:45:07 -0700

    updated footer with anchors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Footer () {
     return (
         <div class="text-center bg-success bg-opacity-50">
-            <h1 class="fs-2">Feel Free to Contact Me 😉</h1>
+            <a href="https://github.com/Chiweenie6" class="m-3"><img src={require("./images/GitHub.jpg")} alt="GitHub" height="50" width="50"></img></a>
+            <a href="https://www.linkedin.com/in/kevin-breedlove-41647793/" class="m-3"><img src={require("./images/Linkedin.png")} alt="Linkedin" height="50" width="50"></img></a>
         </div>
     );
 }
